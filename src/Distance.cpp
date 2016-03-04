a64fe17aef9d8eda677044555c95375fdcfd1590
bennypi <bennyprange@googlemail.com>
2016-03-04 14:25:02 +0100

    error in solvePnPRansac, argument points is wrong

--- src/Distance.cpp
+++ src/Distance.cpp
@@ -29,11 +29,40 @@ Distance::~Distance() {
 }
 
 bool image_detected, found;
+cv::Size boardDims(7, 5);
+float boardSize = 0.03;
 
 void readImage(const sensor_msgs::Image::ConstPtr msgImage, cv::Mat &image) {
 	cv_bridge::CvImageConstPtr pCvImage;
 	pCvImage = cv_bridge::toCvShare(msgImage, msgImage->encoding);
 	pCvImage->image.copyTo(image);
+}
+
+void getDistance(cv::Mat points) {
+	std::vector<cv::Point3f> board;
+	board.resize(boardDims.width * boardDims.height);
+	for (size_t r = 0, i = 0; r < (size_t) boardDims.height; ++r) {
+		for (size_t c = 0; c < (size_t) boardDims.width; ++c, ++i) {
+			board[i] = cv::Point3f(c * boardSize, r * boardSize, 0);
+		}
+	}
+
+	cv::Mat cameraMatrix, distortion, rvec, rotation, translation;
+	;
+	cv::FileStorage fs;
+	std::string path("/home/benny/kinect_cal_data/calib_color.yaml");
+	if (fs.open(path, cv::FileStorage::READ)) {
+		fs["cameraMatrix"] >> cameraMatrix;
+		fs["distortionCoefficients"] >> distortion;
+		fs.release();
+	} else {
+		std::cerr << "couldn't read calibration '" << path << "'!" << std::endl;
+	}
+	cv::solvePnPRansac(board, points, cameraMatrix, distortion, rvec,
+			translation, false, 300, 0.05, board.size(), cv::noArray(),
+			cv::ITERATIVE);
+
+	std::cout << translation << std::endl;
 }
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
@@ -43,12 +72,15 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 	readImage(msg, mat);
 
 //	std::cout << mat << std::endl;
-	cv::imshow("Display window", mat);
 
-	cv::Size boardDims(7, 5);
 	found = cv::findChessboardCorners(mat, boardDims, output,
 			cv::CALIB_CB_FAST_CHECK);
 	std::cout << found << std::endl;
+	if (found) {
+		getDistance(mat);
+		cv::drawChessboardCorners(mat, boardDims, output, found);
+		cv::imshow("Display window", mat);
+	}
 }
 
 int main(int argc, char **argv) {
